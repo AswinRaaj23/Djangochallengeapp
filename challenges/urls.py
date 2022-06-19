13c659eb973b6f6225bba4e97a1cd14cfc3b9a7f
AswinRaaj R I <aswinraaj26@gmail.com>
2022-06-19 13:03:53 +0530

    Return Html without standalone templates

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
+    path("", views.home),
     path('<int:month>/', views.monthly_challenge_by_number),
     path('<str:month>/', views.monthly_challenge, name="month-chall"),
 ]
