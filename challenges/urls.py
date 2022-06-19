8e266a2a9e22944cb876202c1a092d80676dbece
AswinRaaj R I <aswinraaj26@gmail.com>
2022-06-19 18:02:22 +0530

    Learn include template tag

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-    path("", views.home),
+    path("", views.home, name="home"),
     path('<int:month>/', views.monthly_challenge_by_number),
     path('<str:month>/', views.monthly_challenge, name="month-chall"),
 ]
